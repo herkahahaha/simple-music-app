af7ea7e9336de112630035aa91f859f06ccbb35a
Herka Trya Fitra <herkahahaha@gmail.com>
2022-01-31 03:55:17 +0700

    add head, headers and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import Headers from './components/Headers'
+import Footer from './components/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Simple Music App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Headers />
+      <div className="md:container mx-24 my-40">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
